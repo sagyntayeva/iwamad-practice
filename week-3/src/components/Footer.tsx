6a8f6d32bacc2add8b412933b0face90eb1c0096
sagyntayeva <r_sagyntayeva@kbtu.kz>
2026-09-20 00:47:06 +0500

    Week 3: profile card as React components

---
+++ week-3/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="w-full py-6 px-4 text-center text-sm text-slate-400">
+      1 &amp; TypeScript
+    </footer>
+  );
+}
+
+export default Footer;
